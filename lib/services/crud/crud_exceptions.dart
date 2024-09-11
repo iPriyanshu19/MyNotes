9664016a7a10bd0cae26c2a5d8ef0254bb102222
iPriyanshu01 <priyanshusharma.ms@outlook.com>
2024-09-11 23:30:44 +0530

    complete chapter 35

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -15,3 +15,5 @@ class CouldNotFindNote implements Exception {}
 class UserAlreadyExist implements Exception {}
 
 class CouldNotUpdateNote implements Exception {}
+
+class UserShouldBeSetBeforeReadingAllNotes implements Exception {}
