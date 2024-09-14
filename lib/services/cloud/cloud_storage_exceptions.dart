fe043af84401364947e0480e7eec742b7d93f344
iPriyanshu19 <priyanshusharmakch@gmail.com>
2024-09-14 23:00:49 +0530

    complete chapter 36

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,11 @@
+class CloudStorageExceptions implements Exception {
+  const CloudStorageExceptions();
+}
+
+class CouldNotCreateNoteException implements Exception {}
+
+class CouldNotGetAllNotesException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
